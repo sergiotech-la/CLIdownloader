66255d94a78c18feb7e5deb1502bc1847ebcc8ea
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-12-07 09:22:36 -0500

    Add missing dependencies and fix generate_unique_name method

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source 'https://rubygems.org'
 gem 'csv'        # For CSV file handling
 gem 'open-uri'   # For opening URLs
 gem 'rubocop'    # For code linting
+gem 'fileutils'
+gem 'readline'
--- cli_downloader.rb
+++ cli_downloader.rb
@@ -2,37 +2,53 @@
 
 require 'csv'
 require 'open-uri'
+require 'fileutils'
+require 'readline'
+require 'pathname'
 
 def download_images_from_csv(csv_file, destination_folder)
-  Dir.mkdir(destination_folder) unless Dir.exist?(destination_folder)
+  FileUtils.mkdir_p(destination_folder) unless Dir.exist?(destination_folder)
+
   CSV.foreach(csv_file, headers: true) do |row|
     product = row['product']
     image_url = row['url']
+    image_extension = File.extname(image_url)
     image_name = product.downcase.gsub(/\s+/, '_')
-    image_path = File.join(destination_folder, generate_unique_name(image_name))
-    open(image_path, 'wb') do |file|
-      file << open(image_url).read
+    unique_name = generate_unique_name(destination_folder, image_name, image_extension)
+    image_path = File.join(destination_folder, unique_name)
+
+    begin
+      open(image_path, 'wb') do |file|
+        file << URI.open(image_url).read
+      end
+      puts "Downloaded #{product} to #{image_path}"
+    rescue StandardError => e
+      puts "Failed to download #{product} from #{image_url}: #{e.message}"
     end
-    puts "Downloaded #{product} to #{image_path}"
-    sleep(1) # Add a 1 second delay between downloads
+
+    sleep(1) # Add a 1-second delay between downloads
   end
 end
 
-def generate_unique_name(name)
+def generate_unique_name(destination_folder, name, extension)
   count = 0
-  unique_name = name
-  while File.exist?(File.join('images', unique_name))
-    unique_name = "#{name}_#{count}"
+  unique_name = "#{name}#{extension}"
+
+  while File.exist?(File.join(destination_folder, unique_name))
     count += 1
+    unique_name = "#{name}_#{count}#{extension}"
   end
+
   unique_name
 end
 
+# Enable tab autocompletion for destination folder
+destination_folder = Readline.readline('Enter the destination folder path: ', true)
+destination_folder.gsub!(/^~/, Dir.home) if destination_folder.start_with?('~')
+
+# Usage
 puts 'Enter the CSV file path:'
 csv_file = gets.chomp
-
-puts 'Enter the destination folder path:'
-destination_folder = gets.chomp
 
 download_images_from_csv(csv_file, destination_folder)
 puts 'Images downloaded successfully!'
